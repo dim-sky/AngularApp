61e8ca21f77fa90864caccaad4e2a487c97c2ea2
dimsky <skylakos99@hotmail.com>
2024-12-26 17:41:57 -0800

    admin page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 export const routes: Routes = [
-    { path: "", component: HomePageComponent },
+    { path: "home", component: HomePageComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterPageComponent },
+    { path: 'admin', component: AdminPageComponent },
+    { path: '**', component: LoginComponent }
 ];
---
+++ src/app/components/admin-page/admin-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-admin-page',
+  imports: [NavbarComponent],
+  templateUrl: './admin-page.component.html',
+  styleUrl: './admin-page.component.css'
+})
+export class AdminPageComponent {
+
+}
